0500a66dfbabb6a8e22ef068a6cdcbc40f297678
thayna <arraisthayna2106@gmail.com>
2023-11-08 14:24:01 -0300

    tailwind config

--- bloco-bicho-preguica/tailwind.config.js
+++ bloco-bicho-preguica/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{tsx, html, css}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue-bp': '#00AFEF',
+        'pink-bp': '#FF0066',
+        'purple-bp': '#9747FF',
+        'yellow-bp': '#F5C000'
+      }
+    },
   },
   plugins: [],
 }
