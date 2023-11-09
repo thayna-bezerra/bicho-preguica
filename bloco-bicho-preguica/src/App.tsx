3c45e8502cf1581f3922044aaa352e2b75f9d8c7
thayna <arraisthayna2106@gmail.com>
2023-11-09 15:09:42 -0300

    order project changed

--- bloco-bicho-preguica/src/App.tsx
+++ bloco-bicho-preguica/src/App.tsx
@@ -1,10 +1,15 @@
-export function App() {
+// src/App.tsx
+import React from 'react';
+import './index.css'; // Certifique-se de importar o arquivo de estilos do Tailwind
 
+function App() {
   return (
-    <>
-      <div className="bg-purple-bp text-white p-4">
-        Bloco Bicho Preguiça
-      </div>
-    </>
-  )
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-2xl font-bold mb-4">Meu Projeto React com Tailwind</h1>
+      <p className="text-lg">Bem-vindo ao meu aplicativo!</p>
+      <button className="bg-green-500 text-white px-3 py-2 mt-4 hover:bg-green-600">Clique Aqui</button>
+    </div>
+  );
 }
+
+export default App;
--- bloco-bicho-preguica/tailwind.config.js
+++ bloco-bicho-preguica/tailwind.config.js
@@ -1,15 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{tsx, html, css}"],
+module.exports =  {
+  content: ["./src/**/*.{html,tsx, ts}"],
   theme: {
-    extend: {
-      colors: {
-        'blue-bp': '#00AFEF',
-        'pink-bp': '#FF0066',
-        'purple-bp': '#9747FF',
-        'yellow-bp': '#F5C000'
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
