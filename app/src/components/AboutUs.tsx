a62e32293ac28266252751e3f057e0bc1763156e
thayna-bezerra <arraisthayna2106@gmail.com>
2023-12-29 21:43:48 -0300

    adding bicho preguica material

--- app/src/components/AboutUs.tsx
+++ app/src/components/AboutUs.tsx
@@ -7,7 +7,13 @@ export function AboutUs() {
                 <p className="uppercase text-lg md:text-2xl pb-5 font-bold text-white">
                     Sobre nós
                 </p>
-                <p className="text-sm md:text-lg px-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium iure incidunt nulla obcaecati nobis sunt excepturi quas perferendis doloremque quia, dolorum expedita! Ipsum molestiae magni minima? Voluptatum, eligendi sint? Sed?</p>
+                <p className="text-sm md:text-lg px-10">Sejam todos bem-vindos ao Carnaval do Bicho Preguiça um bloco que há mais de uma década encanta e agita os corações dos foliões apaixonados por essa festa tão brasileira.
+                    Tornou-se o queridinho dos amantes do Carnaval, graças à sua estrutura única, atrações espetaculares e compromisso inabalável com a diversão e segurança de todos os participantes.
+
+                    Estamos ansiosos para receber, em 2024, tanto a geração raiz que já faz parte dessa história, quanto a nova geração, pronta para tornar nosso bloco ainda mais vibrante e diferenciado. Venham se juntar a nós para fazer deste Carnaval um momento único, repleto de alegria, música e energia contagiante.
+
+                    É hora de celebrar juntos e fazer deste bloco um espetáculo inesquecível para todos! 
+                </p>
             </div>
             <div className="w-full">
                 <img className="w-full h-full object-cover" src={sobre} alt="sobre nos" />
