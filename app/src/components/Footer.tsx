ab4ed88835a932a6f6e4d4d31aa245c8c9dba09b
thayna-bezerra <arraisthayna2106@gmail.com>
2024-01-06 14:41:58 -0300

    fix bug link wpp sponsor

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -21,7 +21,7 @@ export function Footer() {
           </div>
 
           <button 
-              onClick={() => window.open('https://api.whatsapp.com/send?phone=+5599%208405-0108&text=Olá%2C+tudo+bom%3F+Eu+gostaria+de+ser+um+patrocinador+do+%2ABicho+Preguiça%2A%21+🦥🎉')}
+              onClick={() => window.open('https://w.app/CZbc23')}
               className="bg-yellow-bp text-white rounded-md uppercase text-base font-bold md:w-full p-4 mt-8"
             >
               Seja um patrocinador
