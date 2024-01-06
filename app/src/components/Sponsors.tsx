356a9ff2388a4a99b11abe6bb2f4712b1cd5df28
thayna-bezerra <arraisthayna2106@gmail.com>
2024-01-06 11:45:25 -0300

    change style sponsors section

--- app/src/components/Sponsors.tsx
+++ app/src/components/Sponsors.tsx
@@ -34,12 +34,12 @@ export function Sponsors() {
   
     return (
         <div className="bg-purple-bp h-auto py-8">
-            <p className="uppercase text-lg md:text-2xl p-4 font-bold text-white">
+            <p className="uppercase text-lg md:text-2xl pt-4 pl-4 font-bold text-white">
                 Patrocinadores oficiais
             </p>
   
             <div className="grid grid-cols-1 md:grid-cols-3">
-                <div className="order-2 md:order-1 md:col-span-1 p-4 flex flex-col justify-center items-center">
+                <div className="order-2 md:order-1 md:col-span-1 px-4 flex flex-col justify-center items-center">
                     <button 
                         onClick={() => window.location.href='https://api.whatsapp.com/send?phone=+5599%209631-8324&text=Olá%2C+tudo+bom%3F+Eu+gostaria+de+ser+um+patrocinador+do+%2ABicho+Preguiça%2A%21+🦥🎉'} 
                         className="blinking-button m-5 uppercase md:font-extrabold md:text-xl font-bold text-base w-full border-none p-3 py-7 bg-yellow-bp text-white border-2"
@@ -49,7 +49,7 @@ export function Sponsors() {
                     </button>
                 </div>
 
-                <Slider className="order-1 md:order-2 flex justify-around md:col-span-2 p-4 gap-4" {...sliderPatrocinadores}>
+                <Slider className="order-1 md:order-2 flex justify-around md:col-span-2 md:mt-8 md:p-4 gap-4" {...sliderPatrocinadores}>
                     <div className="flex justify-around md:col-span-2 p-4 gap-4">
                         <img className="w-full h-full" src={p1} alt="p1" />
                     </div>
